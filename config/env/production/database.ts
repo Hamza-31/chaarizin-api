6c48a4e9a6824d35179bf23c53ed5207cd2d5f67
Hamza <hamza.labzioui@gmail.com>
2023-06-29 16:40:14 +0200

    add database: JAWS_DB

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,16 +1,13 @@
 import parseUrl from 'parse-url';
-// import knex from 'knex';
-const config = parseUrl(process.env.CLEARDB_DATABASE_URL)
+const config = parseUrl(process.env.JAWSDB_URL)
 module.exports = ({ env }) => {
-	// const { protocol:client,host, port, database, user, password } = parseUrl(env("CLEARDB_DATABASE_URL"));
-
 	return {
 		connection: {
 			client: config['protocol'],
 			connection: {
 				host: config['host'],
 				port: config['port'],
-				database: config['database'],
+				database: process.env.JAWS_DB,
 				user: config['user'],
 				password: config['password'],
 				ssl: { rejectUnauthorized: false },
